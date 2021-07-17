51f46d752b3f847802cc42ab5258e209a28cd351
geetha sohita <geethasohita@gmail.com>
2021-07-17 14:13:01 -0500

    mongoDB connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+mongodb_client = PyMongo(app, uri="mongodb://localhost:27017/myretail")
+db = mongodb_client.db
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from app import app
+from app import app, db
 
 
 @app.route('/')
 def index():
+    db.example.insert_one({'message': 'test message'})
     return 'Welcome to myRetail'
